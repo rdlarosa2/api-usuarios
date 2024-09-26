27aa58aad40d17662d7717b40ef55b9f32ae3962
rdlarosa2 <rdlarosa2@gmail.com>
2024-09-26 16:41:27 -0500

    Deleting unnecessary file

--- src/main/java/com/users/api/exceptions/PokemonNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.users.api.exceptions;
-
-public class PokemonNotFoundException extends RuntimeException {
-    private static final long serialVerisionUID = 1;
-
-    public PokemonNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/users/api/exceptions/ReviewNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.users.api.exceptions;
-
-public class ReviewNotFoundException extends RuntimeException {
-    private static final long serialVerisionUID = 2;
-    public ReviewNotFoundException(String message) {
-        super(message);
-    }
-}
